6a6d51a06b8d340603c85d4042bef71e3ce5e0c2
himanshu272 <himanshu.shekharjha.272@gmail.com>
2019-03-12 19:26:33 +0530

    second commit

--- codechef.py
+++ codechef.py
@@ -4,23 +4,37 @@ import os
 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
+
+IS_FIREFOX = True
  
-profile = webdriver.FirefoxProfile('/home/himanshu/.mozilla/firefox/bjru6n7m.default')
-driver = webdriver.Firefox(executable_path='./geckodriver',firefox_profile=profile)
+if IS_FIREFOX:
+	profile = webdriver.FirefoxProfile('/home/himanshu/.mozilla/firefox/bjru6n7m.default')
+	driver = webdriver.Firefox(executable_path='./geckodriver',firefox_profile=profile)
+
+else:
+	options = webdriver.ChromeOptions()
+	options.add_argument('user-data-dir=/home/himanshu/.config/\google-chrome/')
+	options.headless = True
+	driver = webdriver.Chrome(executable_path = '../chrome/chromedriver', options =options)
 
 driver.implicitly_wait(60)
 
 prob_url=sys.argv[1]
 code_address=sys.argv[2]
+try:
+	driver.get("https://codechef.com/submit/"+prob_url)
+	upload=driver.find_element_by_xpath("//input[@type='file']")
+	upload.send_keys(code_address)
 
-driver.get("https://codechef.com/submit/"+prob_url)
-upload=driver.find_element_by_xpath("//input[@type='file']")
-upload.send_keys(code_address)
-
-submit=driver.find_element_by_xpath("//input[@id='edit-submit-1']")
-driver.execute_script("arguments[0].scrollIntoView();", submit)
-submit.click()
-time.sleep(5)
-driver.close()
+	submit=driver.find_element_by_xpath("//input[@id='edit-submit-1']")
+	driver.execute_script("arguments[0].scrollIntoView();", submit)
+	submit.click()
+	time.sleep(5)
+except Exception as e:
+	f = open("error.log","a")
+	print("Error occured.\n Check the error.log file")
+	f.close()
+finally:
+	driver.close()
 
 
